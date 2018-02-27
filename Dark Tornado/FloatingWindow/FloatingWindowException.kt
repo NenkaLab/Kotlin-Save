d9d49e307236c1d2687dd9e01bc69230baf82f93
Nenka <nenka@naver.com>
2018-02-27 15:49:16 +0900

    Create FloatingWindowException.kt

---
+++ Dark Tornado/FloatingWindow/FloatingWindowException.kt
@@ -0,0 +1,20 @@
+//package com.example.permissions; your app's package name
+
+/**
+ * --------NenkaChat--------
+ * Created by nenka
+ * Date : 2018-02-27 (2월 / 화요일)
+ * Time : 오후 2:44
+ * ---------------------------
+ */
+class FloatingWindowException : Exception {
+
+    constructor() : super()
+
+    constructor(message: String) : super(message)
+
+    constructor(message: String, throwable: Throwable) : super(message, throwable)
+
+    constructor(cause: Throwable) : super(cause)
+
+}
